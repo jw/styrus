b4edc0eb19ccdbbae6ad551aa0d33ef8c0848c76
Jan Willems <jw@elevenbits.com>
2021-08-18 09:26:01 +0200

    mies: added indent and selector and property to the ast.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use clap::Clap;
 
 use pest::error::Error;
 use pest::Parser;
+use pest::iterators::Pair;
 
 #[derive(Parser)]
 #[grammar = "indent.pest"]
@@ -17,12 +18,15 @@ struct StylusParser;
 pub enum AstNode {
     Property {
         name: String,
-        value: String,
+        values: Vec<String>,
     },
     Selector {
-        ident: String,
-        properties: Vec<AstNode>,
+        word: String,
     },
+    Rule {
+        selector: Box<AstNode>,
+        properties: Vec<AstNode>,
+    }
 }
 
 #[derive(Clap)]
@@ -40,7 +44,7 @@ fn main() {
     }
     let unparsed_file = fs::read_to_string(opts.source).expect("cannot read stylus file");
     let astnode = parse(&unparsed_file).expect("unsuccessful parse");
-    println!("Result: {:?}", &astnode);
+    println!("Result: {:#?}", &astnode);
 }
 
 fn parse(source: &str) -> Result<Vec<AstNode>, Error<Rule>> {
@@ -50,29 +54,85 @@ fn parse(source: &str) -> Result<Vec<AstNode>, Error<Rule>> {
 
     for rule in rules.into_inner() {
         println!("rule: {:?}", rule);
+        match rule.as_rule() {
+            Rule::rule => {
+                ast.push(create_rule(rule));
+            }
+            _ => unreachable!(),
+        }
     }
-    // for token in rules.tokens() {
-    //     println!("token: {:?}", token);
-    // }
-
-
-    // for rule in rules.into_inner() {
-    //     match rule.as_rule() {
-    //         Rule::rule => {
-    //             let mut inner_rules = rule.into_inner();
-    //             let selector = AstNode::Selector {
-    //                 ident: inner_rules.next().unwrap().as_str().to_string(),
-    //                 properties: build_ast_from_property_lines(inner_rules),
-    //             };
-    //             ast.push(selector);
-    //         }
-    //         Rule::EOI => (),
-    //         _ => unreachable!(),
-    //     }
-    // }
 
     Ok(ast)
 }
+
+fn create_rule(rule: Pair<Rule>) -> AstNode {
+    println!("rule: {:?}", rule);
+    let mut inner_rules = rule.into_inner();
+    println!("Inner rules: {:?}", inner_rules);
+    let rule = AstNode::Rule {
+        selector: Box::from(AstNode::Selector { word: inner_rules.next().unwrap().as_str().to_string() }),
+        properties: create_properties(inner_rules),
+    };
+    rule
+}
+
+fn create_properties(pairs: pest::iterators::Pairs<Rule>) -> Vec<AstNode> {
+    let mut ast = vec![];
+    for pair in pairs {
+        println!("pair: {:?}", pair);
+        match pair.as_rule() {
+            Rule::property => {
+                ast.push(create_property(pair.into_inner()));
+            },
+            Rule::indentation => {
+                println!("Found indentation");
+            }
+            _ => unreachable!(),
+        }
+    }
+    ast
+}
+
+fn create_property(pairs: pest::iterators::Pairs<Rule>) -> AstNode {
+    let mut name = "".to_string();
+    let mut values = vec![];
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::name => {
+                name = pair.as_str().to_string();
+            },
+            Rule::values => {
+                values = create_values(pair.into_inner());
+            },
+            _ => unreachable!(),
+        }
+    }
+    let property = AstNode::Property {
+        name: name,
+        values: values
+    };
+    property
+}
+
+fn create_values(pairs: pest::iterators::Pairs<Rule>) -> Vec<String> {
+    let mut values = vec![];
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::value => {
+                values.push(pair.as_str().to_string());
+            },
+            _ => unreachable!(),
+        }
+    }
+    values
+}
+
+
+    // let selector = AstNode::Rule {
+    //     ident: inner_rules.next().unwrap().as_str().to_string(),
+    //     properties: build_ast_from_property_lines(inner_rules),
+    // };
+
 
 // fn build_ast_from_property_lines(pairs: pest::iterators::Pairs<Rule>) -> Vec<AstNode> {
 //     let mut ast = vec![];
